4be160719fa1cb3e24523e0faec8638eb1626fec
Chinegua <ai.chinegua@gmail.com>
2018-04-17 13:15:17 +0200

    refactoring

--- src/services/habitacion.service.ts
+++ src/services/habitacion.service.ts
@@ -3,7 +3,7 @@ import { Component } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Habitacion } from '../interfaces/habitacion.interface';
 import { CreateHabitacionDto } from '../dtos/create-habitacion.dto';
-import { HabitacionSchema } from '../models/habitacion.schema';
+import { HabitacionSchema } from '../schemas/habitacion.schema';
 
 @Component()
 export class HabitacionService {
