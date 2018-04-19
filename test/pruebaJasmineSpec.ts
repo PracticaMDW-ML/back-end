b5b2d577e17260f3f610d3f9db8184ed1a815069
Chinegua <ai.chinegua@gmail.com>
2018-04-19 23:01:02 +0200

    mocha e2e test

--- test/pruebaJasmineSpec.ts
+++ test/pruebaJasmineSpec.ts
@@ -3,13 +3,20 @@ import { HotelService } from '../src/hotel/hotel.service';
 import { HotelSchema } from '../src/hotel/hotel.schema';
 import { Hotel } from '../src/hotel/hotel.interface';
 import { expect } from 'chai';
+import request = require('supertest');
 
 describe('something', () => {
-    const hotelService: HotelService = new HotelService(HotelSchema);
-    const hotelController: HotelController = new HotelController(hotelService);
+    const peticion = request.agent('http://localhost:3008');
+
     it('should work', () => {
+        peticion.get('/hoteles/5ad7a1b15b87860488c0422c')
+        .end((err, res) => {
+            console.log(res.body.nombre);
+            expect('NH Madri').to.equal(res.body.nombre);
+        });
+
         // let hotel: Hotel;
         // hotel =  hotelService.findById('5ad7a1b15b87860488c0422c');
-        expect('afsdg').to.equal(await hotelController.findOne('5ad7a1b15b87860488c0422c'));
+        // expect(1).to.equal(0);
     });
 });
