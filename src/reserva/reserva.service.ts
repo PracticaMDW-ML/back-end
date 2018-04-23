8aa30d8451e53d2290981e7243a8cc6474e15663
AlbertoNitro <a.jimenezmir@alumnos.urjc.es>
2018-04-23 14:54:05 +0200

    Autenticacion + Actualizar reserva OK

--- src/reserva/reserva.service.ts
+++ src/reserva/reserva.service.ts
@@ -4,6 +4,7 @@ import { Reserva } from './reserva.interface';
 import { ReservaSchema } from './reserva.schema';
 import { InjectModel, MongooseModule } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import {AuthService} from 'auth/auth.service';
 
 @Component()
 export class ReservaService {
--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -1,9 +1,10 @@
 import { Model } from 'mongoose';
-import { Component } from '@nestjs/common';
+import {Component, forwardRef, Inject} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Usuario } from './usuario.interface';
 import { UsuarioDto } from './user.dto';
 import { UsuarioSchema } from './usuario.schema';
+import {AuthService} from "../auth/auth.service";
 
 @Component()
 export class UsuarioService {
